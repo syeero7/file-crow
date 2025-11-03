988f0b30dc87500252169423a43631fe3a7001a9
emal <emalhasitha@proton.me>
2025-11-04 03:21:01 +0530

    fix: handle non-OK HTTP status codes within file upload goroutine

--- upload_handler.go
+++ upload_handler.go
@@ -21,37 +21,52 @@ func uploadHandler(fsvr *fileServer, w http.ResponseWriter, r *http.Request) {
 
 	files := r.MultipartForm.File["file"]
 	var wg sync.WaitGroup
+	httpStatus := make(chan int, len(files))
 
 	for _, f := range files {
 		wg.Add(1)
-		go func(h *multipart.FileHeader) {
-			defer wg.Done()
-
-			dstF, err := os.CreateTemp(fsvr.directory, createTempFilename(h.Filename))
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			defer ioCloser(dstF)
-			srcF, err := h.Open()
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			defer ioCloser(srcF)
-			if _, err := io.Copy(dstF, srcF); err != nil {
-				log.Println(err)
-				return
-			}
-
-			log.Printf("file uploaded %s %s\n", h.Filename, humanReadSize(h.Size))
-		}(f)
+		go uploadFiles(f, fsvr.directory, &wg, httpStatus)
 	}
 
 	wg.Wait()
+	close(httpStatus)
+	for code := range httpStatus {
+		if code != http.StatusOK {
+			w.WriteHeader(code)
+			return
+		}
+	}
+
 	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func uploadFiles(h *multipart.FileHeader, dir string, wg *sync.WaitGroup, status chan int) {
+	defer wg.Done()
+
+	dstF, err := os.CreateTemp(dir, createTempFilename(h.Filename))
+	if err != nil {
+		status <- http.StatusBadRequest
+		log.Println(err)
+		return
+	}
+
+	defer ioCloser(dstF)
+	srcF, err := h.Open()
+	if err != nil {
+		status <- http.StatusInternalServerError
+		log.Println(err)
+		return
+	}
+
+	defer ioCloser(srcF)
+	if _, err := io.Copy(dstF, srcF); err != nil {
+		status <- http.StatusInternalServerError
+		log.Println(err)
+		return
+	}
+
+	status <- http.StatusOK
+	log.Printf("file uploaded %s %s\n", h.Filename, humanReadSize(h.Size))
 }
 
 func createTempFilename(name string) string {
