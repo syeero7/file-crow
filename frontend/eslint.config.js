56ebf6e1d21e132cce5fee85e3f4c2282c330daf
emal <emalhasitha@proton.me>
2026-07-01 19:42:07 +0530

    chore: reinitialize frontend

---
+++ frontend/eslint.config.js
@@ -0,0 +1,37 @@
+import svelteConfig from "./svelte.config.js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import ts from "typescript-eslint";
+import svelte from "eslint-plugin-svelte";
+
+export default defineConfig(
+  ts.configs.recommended,
+  ts.configs.recommended,
+  svelte.configs.all,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: [".svelte"],
+        parser: ts.parser,
+        // Note: `eslint --cache` will fail with non-serializable properties.
+        // In those cases, please remove the non-serializable properties.
+        // svelteConfig: {
+        //   ...svelteConfig,
+        //   }
+        svelteConfig,
+      },
+    },
+  },
+  {
+    rules: {},
+  },
+);
