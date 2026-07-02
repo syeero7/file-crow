2d238951e2101b9437213d71cc663a351d4b2e8e
emal <emalhasitha@proton.me>
2026-07-02 07:27:34 +0530

    build: configure vite dev mode server proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,27 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import path from "path";
 
-export default defineConfig({
-  plugins: [svelte()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+const PORT = 8080;
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [svelte()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    ...(mode.startsWith("dev") && {
+      server: {
+        proxy: {
+          "/api": {
+            target: `http://localhost:${PORT}`,
+            changeOrigin: true,
+            secure: false,
+            rewrite: (path) => path.replace(/^\/api/, ""),
+          },
+        },
+      },
+    }),
+  };
 });
