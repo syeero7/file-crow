227109ac2a221a62d5bea81ebc5c3f4afcb9121a
emal <emalhasitha@proton.me>
2026-07-03 19:57:29 +0530

    build: fix pnpm build command

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 set -e
 
 echo "Installing dependencies..."
-go mod tidy \
+go mod tidy
 pnpm --dir frontend install
 
 echo "Building the frontend..."
-pnpm --dir frontend run build
+pnpm --dir frontend build
 
 echo "Compiling..."
 CGO_ENABLED=0 go build -o ./bin/filecrow -trimpath -ldflags='-s -w'
